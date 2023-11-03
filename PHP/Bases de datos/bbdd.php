0518dbe80d4ee551b4affcc0a49984a5e4dee06a
Dani-mk49 <danielalvarorio@gmail.com>
2023-11-03 20:57:04 +0100

    kjdhfdkljhfklhfe

---
+++ PHP/Bases de datos/bbdd.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Document</title>
+</head>
+<body>
+<?php
+include 'consultasBasicas.php';
+?>
+</body>
+</html>
---
+++ PHP/Bases de datos/consultasBasicas.php
@@ -0,0 +1,45 @@
+<?php
+
+global $hostname;
+global $database;
+global $username;
+global $password;
+global $conexion;
+
+function datosConexion($host, $db, $user, $pass)
+{
+    $hostname = $host;
+    $database = $db;
+    $username = $user;
+    $password = $pass;
+}
+
+function conectar()
+{
+    if(!isset($hostname) && !isset($database) && !isset($username) && !isset($password)) {
+        $conexion = new mysqli($hostname, $username, $password, $database);
+    }
+    if($conexion->connect_error) {
+        exit("La conexion no se pudo realizar el fallo es: " . $conexion->connect_error);
+    }
+}
+
+function desconectar()
+{
+    $conexion->close();
+}
+
+function verTable($nombreTabla)
+{
+    try {
+        $sentencia = $conexion->prepare("SELECT * FROM ?");
+        $sentencia->bind_param('s', $nombreTabla);
+        $sentencia->execute();
+        $resultado = $sentencia->get_result();
+        return $resultado;
+    } catch (Excepcion $excepcion) {
+        exit("Error al obtener el resultado: " . $excepcion);
+    } finally {
+        desconectar();
+    }
+}
