9bead4d6b591c75fcd1cab2f91f941c06f2a8c68
Dani-mk49 clase <danielalvarorio@gmail.com>
2023-11-30 20:54:34 +0100

    asdf

--- PHP/sitio web dinamico con php/includes/header.php
+++ PHP/sitio web dinamico con php/includes/header.php
@@ -1,6 +1,6 @@
 <header>
-  <div>
-    <img src="" alt="imagen pocha">
+  <div class="logo">
+    <a href="index.php"><img src="images/logo.jpg" alt="imagen pocha"></a>
   </div>
   <h1>DESARROLLO WEB EN ENTORNO SERVIDOR</h1>
 </header>
---
+++ PHP/sitio web dinamico con php/includes/header2.php
@@ -0,0 +1,6 @@
+<header>
+  <div>
+    <img src="../images/logo.jpg" alt="imagen pocha">
+  </div>
+  <h1>DESARROLLO WEB EN ENTORNO SERVIDOR</h1>
+</header>
