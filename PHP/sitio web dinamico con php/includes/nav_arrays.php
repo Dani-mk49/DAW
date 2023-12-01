cce44732637704a4a6431b637a240df54f626dc5
Dani-mk49 clase <danielalvarorio@gmail.com>
2023-12-01 15:17:22 +0100

    mover archivos

--- PHP/sitio web dinamico con php/includes/nav_arrays.php
+++ PHP/sitio web dinamico con php/includes/nav_arrays.php
@@ -4,8 +4,14 @@
       <li class="menuListItem"><a href="">INICIO</a></li>
       <li class="menuListItem"><a href="ejer1.php">Ejercicio 1</a></li>
       <li class="menuListItem"><a href="ejer2.php">Ejercicio 2</a></li>
-      <li class="menuListItem"><a href="ejer3.php">Ejercicio 3</a></li>
+      <li class="menuListItem"><a href="ejer3from.php">Ejercicio 3</a></li>
       <li class="menuListItem"><a href="ejer4.php">Ejercicio 4</a></li>
       <li class="menuListItem"><a href="ejer5.php">Ejercicio 5</a></li>
+      <li class="menuListItem"><a href="ejer6.php">Ejercicio 6</a></li>
+      <li class="menuListItem"><a href="ejer7.php">Ejercicio 7</a></li>
+      <li class="menuListItem"><a href="ejer8.php">Ejercicio 8</a></li>
+      <li class="menuListItem"><a href="ejer9.php">Ejercicio 9</a></li>
+      <li class="menuListItem"><a href="ejer10.php">Ejercicio 10</a></li>
+      <li class="menuListItem"><a href="ejer11.php">Ejercicio 11</a></li>
     </ul>
   </nav>
