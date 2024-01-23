1311236cc9e282c989d0bbb53723c75863b210e3
Dani-mk49 clase <danielalvarorio@gmail.com>
2024-01-23 17:47:14 +0100

    retoques esteticos

--- PHP/Tema 11 ejercicios OOP/ejercicio3.php
+++ PHP/Tema 11 ejercicios OOP/ejercicio3.php
@@ -13,6 +13,7 @@
   }
 
   .reglas {
+    width: 300px;
   }
 
   .operacion {
@@ -21,10 +22,10 @@
 
   .resultado {
     padding: 30px;
+    width: 325px;
   }
 
   .Tresultado {
-    /*display: none;*/
     border-collapse: collapse;
   }
 
@@ -37,7 +38,9 @@
     color: white;
     background-color: darkblue;
   }
-
+  .general{
+    padding: 40px;
+  }
 </style>
 
 <body>
@@ -45,82 +48,89 @@
   <div class="contenedorCentral">
     <?php include 'nav.php'; ?>
     <main>
+    <table style="margin: 0 auto;" >
+    <tr>
+      <td valign="top" rowspan="2" class="general">
 
         <fieldset class="especialDiv reglas">
           <legend><b>Reglas de uso de la calculadora</b></legend>
 
-        <ul>
-          <li>La calculadora se usa escribiendo la operacion completa.</li>
-          <li>La operación será <b>Operando_1 operación Operando_2</b></li>
-          <li>Cada operando puede ser un número positivo <b>entero</b> o <b>racional</b></li>
-          <li>Los operadore racionales permitidos son <span style="color: blue;">+ - * :</span></li>
-          <li>No se deben dejar espacios en blanco entre operandos y operación</li>
-          <li>Ejemplos:
-            <ul>
-              <li>5+4</li>
-              <li>5/2:2</li>
-              <li>1/4*2/3</li>
-              <li>2/7:1/3</li>
-            </ul>
-          </li>
-        </ul>
+          <ul>
+            <li>La calculadora se usa escribiendo la operacion completa.</li>
+            <li>La operación será <b>Operando_1 operación Operando_2</b></li>
+            <li>Cada operando puede ser un número positivo <b>entero</b> o <b>racional</b></li>
+            <li>Los operadore racionales permitidos son <span style="color: blue;">+ - * :</span></li>
+            <li>No se deben dejar espacios en blanco entre operandos y operación</li>
+            <li>Ejemplos:
+              <ul>
+                <li>5+4</li>
+                <li>5/2:2</li>
+                <li>1/4*2/3</li>
+                <li>2/7:1/3</li>
+              </ul>
+            </li>
+          </ul>
         </fieldset>
+      </td>
+      <td class="general">
+
         <fieldset class="especialDiv operacion">
           <legend><b>Establece la operación</b></legend>
           <form action="ejercicio3.php">
             Operación:
             <input type="text" name="operacion" required>
             <input type="submit" value="Calcular" name="calcular">
-          <?php
-          $resultado;
-          $operando1;
-          $operando2;
-          $signo;
-  if (isset($_REQUEST['operacion'])) {?>
+            <?php
+
+if (isset($_REQUEST['operacion'])) {?>
             <br>
             <span style="color: blue; font-weight: bold;">
-<?php
+              <?php
       // Divide la cadena en un array usando múltiples delimitadores
       $numeros = preg_split('/[+:*-]/', $_REQUEST['operacion']);
 
-      // Filtra elementos vacíos que pueden surgir debido a delimitadores consecutivos
-      $numeros = array_filter($numeros);
+    // Filtra elementos vacíos que pueden surgir debido a delimitadores consecutivos
+    $numeros = array_filter($numeros);
 
-      // Utiliza preg_match para encontrar el primer separador en la cadena
-      preg_match('/[+:*-]/', $_REQUEST['operacion'], $matches);
+    // Utiliza preg_match para encontrar el primer separador en la cadena
+    preg_match('/[+:*-]/', $_REQUEST['operacion'], $matches);
 
-      // Utiliza el primer carácter encontrado como separador
-      $signo = $matches[0];
-      $operando1 = new Racional($numeros[0]);
-      $operando2 = new Racional($numeros[1]);
-      echo '<br>';
-      switch ($signo) {
-          case '+':
-              $resultado = $operando1->sumar($operando2);
-              break;
-          case '-':
-              $resultado = $operando1->restar($operando2);
-              break;
-          case '*':
-              $resultado = $operando1->multiplicar($operando2);
-              break;
-          case ':':
-              $resultado = $operando1->dividir($operando2);
-              break;
-      }
+    // Utiliza el primer carácter encontrado como separador
+    $signo     = $matches[0];
+    $operando1 = new Racional($numeros[0]);
+    $operando2 = new Racional($numeros[1]);
+    print '<br>';
+    switch ($signo) {
+        case '+':
+            $resultado = $operando1->sumar($operando2);
+            break;
+        case '-':
+            $resultado = $operando1->restar($operando2);
+            break;
+        case '*':
+            $resultado = $operando1->multiplicar($operando2);
+            break;
+        case ':':
+            $resultado = $operando1->dividir($operando2);
+            break;
+    }
 
-      // Imprime el resultado directamente
-      echo $operando1->__toString() . $signo . $operando2->__toString(). '=' . $resultado->__toString();
+    // Imprime el resultado directamente
+    print $operando1->__toString() . $signo . $operando2->__toString() . '=' . $resultado->__toString();
 
-
-  }
+}
   ?>
           </span>
-          </form>
-        </fieldset>
-      <fieldset class="especialDiv resultado">
-        <legend><b>Resultado</b></legend>
-        <?php
+        </form>
+      </fieldset>
+    </td>
+    </tr>
+    <tr>
+      <td class="general">
+
+        <fieldset class="especialDiv resultado">
+          <legend><b>Resultado</b></legend>
+          <?php
 if (isset($_REQUEST['operacion'])) {
     ?>
         <table class="Tresultado">
@@ -132,7 +142,7 @@ if (isset($_REQUEST['operacion'])) {
             <td><b>Operando 1</b></td>
             <td align="center">
               <?php
-              echo $operando1->__toString();
+                  print $operando1->__toString();
     ?>
             </td>
           </tr>
@@ -140,34 +150,34 @@ if (isset($_REQUEST['operacion'])) {
             <td><b>Operando 2</b></td>
             <td align="center">
               <?php
-    echo $operando2->__toString();
+    print $operando2->__toString();
     ?>
               </td>
-          </tr>
-          <tr>
-            <td><b>Operación</b></td>
-            <td align="center">
-              <?php
-    echo $signo;
+            </tr>
+            <tr>
+              <td><b>Operación</b></td>
+              <td align="center">
+                <?php
+    print $signo;
     ?>
     </td>
-          </tr>
-          <tr>
-            <td><b>Resultado</b></td>
-            <td align="center">
-              <?php
-    echo $resultado->__toString();
+  </tr>
+  <tr>
+    <td><b>Resultado</b></td>
+    <td align="center">
+      <?php
+    print $resultado->__toString();
     ?>
 
-            </td>
-          </tr>
-          <tr>
-            <td><b>Resultado simplificado</b></td>
-            <td align="center">
-              <?php
+</td>
+</tr>
+<tr>
+  <td><b>Resultado simplificado</b></td>
+  <td align="center">
+    <?php
               $resultado->simplificar();
-              echo $resultado->__toString();
-              ?>
+    print $resultado->__toString();
+    ?>
             </td>
           </tr>
         </table>
@@ -175,7 +185,10 @@ if (isset($_REQUEST['operacion'])) {
 }
   ?>
       </fieldset>
-    </main>
+    </td>
+    </tr>
+    </table>
+  </main>
     <?php include 'aside.php'; ?>
   </div>
   <?php include 'footer.php'; ?>
